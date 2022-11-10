2ceec076a7dae768ccc9c98e9bc84fa91d766ec5
atu816 <alexandertu95@gmail.com>
2022-11-09 22:56:50 -0800

    Some miscellaneous testing files started. Nothing more than files.
    Co-authored-by: James Howat <97859901+jbhowat@users.noreply.github.com>
    Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>
    Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>

---
+++ __tests__/react.js
@@ -0,0 +1,4 @@
+// import React from 'React';
+// import userEvent from '@testing-library/user-event'
+// import { render, screen, waitFor } from '@testing-library/react';
+// import regeneratorRuntime from 'regenerator-runtime';
---
+++ __tests__/supertest.js
@@ -0,0 +1,4 @@
+// const { response } = require('express');
+// const request = require('supertest');
+
+// const server = 'http://localhost:8080/';
